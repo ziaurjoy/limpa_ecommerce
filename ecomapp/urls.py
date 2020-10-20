5371c64a4805dd47511df9d683eac7c6dfcd2b12
joy <ziaurjoy802@gmail.com>
2020-10-20 21:35:41 +0600

    more design of category products

--- ecomapp/urls.py
+++ ecomapp/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import home,single_product
+from .views import home,single_product,filter_of_categories
 urlpatterns = [
     path('', home, name='home'),
     path('product/<int:product_id>', single_product, name='single-product'),
+    path('product/<category_name>', filter_of_categories, name='category-name'),
 ]
--- ecomapp/views.py
+++ ecomapp/views.py
@@ -21,14 +21,30 @@ def home(request):
 
 
 def single_product(request,product_id):
+    categorys = Category.objects.all()
     product = Product.objects.get(id=product_id)
     product_filter = Product.objects.filter(category=product.category)
     product_image = Images.objects.filter(product_id=product_id)
     context = {
+        'categorys': categorys,
         'product': product,
         'product_image': product_image,
         'filter_of_product': product_filter
     }
     return render(request,'fontend/pages/single_product.html',context)
+
+
+def filter_of_categories(request,category_name):
+    product_obj = Product.objects.all().order_by('-id')[:2]
+    categorys = Category.objects.all()
+    category_name = Category.objects.get(title=category_name)
+    filtering_products = Product.objects.filter(category=category_name).order_by('-id')
+    context = {
+        'products': product_obj,
+        'filtering_products': filtering_products,
+        'categorys': categorys,
+        'category_name': category_name
+    }
+    return render(request,'fontend/pages/filter_of_categories.html',context)
 
 
