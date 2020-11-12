a599cb1b6820f9321b436b5c921b0410c4c9a643
joy <ziaurjoy802@gmail.com>
2020-11-12 21:34:09 +0600

    Custom User Login details

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from .views import user_logout,user_login
 urlpatterns = [
-
+    path('logout', user_logout, name='user-logout'),
+    path('login', user_login, name='user-login'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+from product.models import Category
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.warning(request, 'Invalid Username or Password')
+    return render(request, 'fontend/pages/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
